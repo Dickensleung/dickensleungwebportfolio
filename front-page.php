9f4ecdda437d45377c18c574fa429859b2a8de8e
DL <67760003+Dickensleung@users.noreply.github.com>
2022-04-22 15:53:59 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying home page.
+ * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -9,150 +9,109 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package dickensleungportfolio
+ * @package dickensleungwebportfolio
  */
 
 get_header();
 ?>
-	<main id="primary" class="site-main">
-	<?php get_template_part('template-parts/content-nav');?>	
-	<body class="cf">		
-	<section class="content my-waypoint" id="scroll">
-		<div id="leftcol" class="col-left">
-			<div class="description-content">
-				<div class="description-header">
-					<h1>Dickens Leung, </h1>
-					<h2>I am a Front-End Developer from beautiful Vancouver, BC.</h2>
-				</div>
-				
-				
-				<div class="about-me-wrapper" id="about-me-wrapper">
-					<h2><u>About me</u></h2>
-					<div class="about-content">
-						<?php if(function_exists('get_field')): 
-							if(get_field('about_me')):
-								echo '<p class="body-content-row">';
-								the_field('about_me');
-								echo '</p>';
-							endif;
-						?>
 
-						<div class="description-small">
-							<?php
-								if(get_field('about_me_two')):
-									echo '<p class="body-content-column">';
-									the_field('about_me_two');
+	<main id="primary" class="site-main">
+		<?php get_template_part('template-parts/content-nav');?>	
+		<section class="content" id="scroll">
+			
+			<!-- left col-->
+			<div id="leftcol">
+				<div class="description-content">
+					<div class="description-header">
+						<h1>Dickens Leung, </h1>
+						<h2>I am a Front-End Developer from beautiful Vancouver, BC.</h2>
+					</div>
+
+					<div class="about-me-wrapper" id="about-me-wrapper">
+						<h2><u>About me</u></h2>
+						<div class="about-content">
+							<?php if(function_exists('get_field')): 
+								if(get_field('about_me')):
+									echo '<p class="body-content-row">';
+									the_field('about_me');
 									echo '</p>';
 								endif;
 							?>
-							<?php
-								if(get_field('about_me_three')):
-									echo '<p class="body-content-column">';
-									the_field('about_me_three');
-									echo '</p>';
-								endif;
-							endif;?>	
-						</div>
-					</div>
 
-					<div id="social-icons">
-						<?php if(function_exists('get_field')):?>
-						<ul>
-							<li>	
-								<a href="mailto:dickens.leung@outlook.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-inbox"></i></a>
-							</li>
-							<li>
-								<?php if(get_field('second_social_link')):?>
-								<a href="<?php the_field('second_social_link');endif;?>"><i class="fab fa-github"></i></a>
-							</li>
-							<li>
-								<?php if(get_field('third_social_link')):?>
-								<a href="<?php the_field('third_social_link');endif;?>"><i class="fab fa-linkedin-in"></i></a>
-							</li>
-						</ul>
-						<?php endif?>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-
-		<div id="rightcol" class="col-right">
-			<div class="project-card-container">
-				<h2 id="home-section-title">My Work</h2>
-				<?php while(have_posts() ):the_post();?>
-					<?php 
-						$args = array(
-							'post_type'      => 'dl-works',
-							'posts_per_page' => -1,
-							'orderby'		 => 'title',
-							'order'			 => 'ASC',
-						);
-						$query = new WP_Query( $args);
-						
-						
-
-						if ( $query -> have_posts() ):
-							while ($query-> have_posts()) : $query -> the_post();
-								if(function_exists('get_field')):?>
-
-								<div id="project-card-wrapper">	
-									<?php if(get_field('project_title')):
-									echo '<a href='.get_permalink().'>';
-										echo '<h3 id="project-title">';
-										the_field('project_title');
-										echo '</h3>';
-									echo'</a>';
-									endif;
-
-									if(get_field('project_sub_title')):
-										echo '<h4>';
-										the_field('project_sub_title');
-										echo '</h4>';
-									endif;
-
-									if(get_field('project_description')):
-										echo '<p>';
-										the_field('project_description');
+							<div class="description-small">
+								<?php
+									if(get_field('about_me_two')):
+										echo '<p class="body-content-column">';
+										the_field('about_me_two');
 										echo '</p>';
 									endif;
-
-									if(get_field('project_description_two')):
-										echo '<div class="project-tools">';
-											echo '<p>';
-											the_field('project_description_two');
-											echo '</p>';?>
-										<?php echo '</div>';
+								?>
+								<?php
+									if(get_field('about_me_three')):
+										echo '<p class="body-content-column">';
+										the_field('about_me_three');
+										echo '</p>';
 									endif;
-									//displaying custom taxonoies according to custom post from function.php
-									echo custom_taxonomies_terms_links();
-									
-									echo '<div id="bg-image" class="col-right-image">';
-									the_post_thumbnail();
-									echo '</div>';
-									endif;
+								endif;?>	
+							</div>
+						</div>
 
-									echo '<button class="choice-btn"><a href="';
-									if(get_field('live_site_link')) :the_field('live_site_link');
-									endif;
-									echo '"target="#">';
-									echo 'Check it out live ⇛';
-									echo '</a></button>';?>
+						<div id="social-icons">
+							<?php if(function_exists('get_field')):?>
+								<ul>
+									<li>	
+										<a href="mailto:dickens.leung@outlook.com" target="_blank" rel="noopener noreferrer"><i class="fas fa-inbox"></i></a>
+									</li>
+									<li>
+										<?php if(get_field('second_social_link')):?>
+										<a href="<?php the_field('second_social_link');endif;?>"><i class="fab fa-github"></i></a>
+									</li>
+									<li>
+										<?php if(get_field('third_social_link')):?>
+										<a href="<?php the_field('third_social_link');endif;?>"><i class="fab fa-linkedin-in"></i></a>
+									</li>
+								</ul>
+							<?php endif?>
+						</div><!-- End Social icons-->
+					</div><!-- End About Me Wrapper-->
+				</div><!-- End Description Content -->
+			</div><!-- End Left col -->
 
-									
-								</div>
-							<?php endwhile;
-						endif;
-					?>
-				<?php endwhile;?>
-			</div>
-		</div>
-		<?php get_footer();?>
-	</section>
-	</body>
+			<div id="rightcol"  class="col-right">
+				<?php while(have_posts() ):the_post();
+					$args = array(
+						'post_type'      => 'dl-works',
+						'posts_per_page' => -1,
+						'orderby'		 => 'title',
+						'order'			 => 'ASC',
+					);
+					$query = new WP_Query( $args);?>
+					<ul id="project-card-wrapper">
+						<?php if ( $query -> have_posts() ){
+							while ($query-> have_posts()) : $query -> the_post();
+								if ( function_exists( 'get_field' )) :
+									if ( get_field( 'project_title' ) ) {
+									echo '<li>';
+									echo '<a id="project-link" href="' . esc_url( get_permalink() ) . '">';
+									echo '<h3 id="project-title" data-controls="#' . esc_attr(strtolower(get_the_title()) ) . '">' . esc_attr( get_the_title() ) . '</h3>';
+									echo '</a>';
+									echo '</li>';
+									}
+								
+								endif;
+							endwhile;
+						}?>
+					</ul>
+				<?php endwhile; ?><!--end the loop--> 
+
+				<div id="featured-image">
+				
+				</div>
+				
+			</div><!-- End rightcol-->
+		</section><!-- end page content -->
+
 	</main><!-- #main -->
 
 <?php
-
-
-
+get_footer();
